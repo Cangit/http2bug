9618e51b1bfd2adfb6a5e365c603a98b0569443e
Cangit <git@fredric.dev>
2022-01-27 14:02:56 +0100

    Initial

---
+++ server.js
@@ -0,0 +1,17 @@
+import { handler } from "./build/handler.js";
+import http2 from "http2";
+import fs from "fs";
+
+const port = 1337;
+const server = http2.createSecureServer(
+  {
+    key: fs.readFileSync("./certs/localhost-privkey.pem"),
+    cert: fs.readFileSync("./certs/localhost-cert.pem"),
+    allowHTTP1: false,
+  },
+  handler
+);
+
+server.listen(port, () => {
+  console.log("🍱 Server running on port: " + port);
+});
